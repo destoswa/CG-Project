1cc3730acbbc39ab9385a358b73ad9da990d8474
Svannia <75037332+Svanniah@users.noreply.github.com>
2023-03-23 16:01:26 +0100

    j'avais foiré les array, normalement mtn c'est bon

--- GL2_meshes/src/normal_computation.js
+++ GL2_meshes/src/normal_computation.js
@@ -27,7 +27,7 @@ function compute_triangle_normals_and_angle_weights(mesh) {
 		const vert3 = get_vert(mesh, mesh.faces[3*i_face + 2])
 		
 		// Modify the way triangle normals and angle_weights are computed
-		tri_normals.push(vec3.normalize(vec3.create(),vec3.cross(vec3.create(),vec3.subtract(vec3.create(),vert1,vert3),vec3.subtract(vec3.create(),vert1,vert2))))
+		tri_normals.push(vec3.normalize(vec3.create(),vec3.cross(vec3.create(),vec3.subtract(vec3.create(),vert2,vert1),vec3.subtract(vec3.create(),vert3,vert1))))
 		angle_weights.push([
 			vec3.angle(vec3.create(), vec3.subtract(vec3.create(),vert2,vert1), vec3.subtract(vec3.create(),vert3,vert1)), 
 			vec3.angle(vec3.create(), vec3.subtract(vec3.create(),vert3,vert2), vec3.subtract(vec3.create(),vert2,vert1)), 
@@ -57,9 +57,9 @@ function compute_vertex_normals(mesh, tri_normals, angle_weights) {
 		const normal = tri_normals[i_face]
 
 		// Add your code for adding the contribution of the current triangle to its vertices' normals
-		vec3.add(vertex_normals[iv1], vertex_normals[iv1], vec3.scale([0., 0., 0.], normal, angle_weights[iv1]))
-		vec3.add(vertex_normals[iv2], vertex_normals[iv2], vec3.scale([0., 0., 0.], normal, angle_weights[iv2]))
-		vec3.add(vertex_normals[iv3], vertex_normals[iv3], vec3.scale([0., 0., 0.], normal, angle_weights[iv3]))
+		vec3.add(vertex_normals[iv1], vertex_normals[iv1], vec3.scale(vec3.create(), normal, angle_weights[i_face][0]))
+		vec3.add(vertex_normals[iv2], vertex_normals[iv2], vec3.scale(vec3.create(), normal, angle_weights[i_face][1]))
+		vec3.add(vertex_normals[iv3], vertex_normals[iv3], vec3.scale(vec3.create(), normal, angle_weights[i_face][2]))
 
 	}
 
